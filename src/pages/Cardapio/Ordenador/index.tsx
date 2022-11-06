671ffcfbff83b1e8884fa70b370a83aae4cc9591
Filipe Sales <eduardojs999@gmail.com>
2022-11-06 08:46:03 -0500

    ordenar por nome

--- src/pages/Cardapio/Ordenador/index.tsx
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -1,17 +1,44 @@
-import styles from './Ordenador.module.scss'
-import opcoes from './opcoes.json'
+import styles from './Ordenador.module.scss';
+import opcoes from './opcoes.json';
+import { useState } from 'react';
+import classNames from 'classnames';
+import {MdKeyboardArrowUp, MdKeyboardArrowDown} from 'react-icons/md';
 
-export default function Ordernador() {
+type IOrdenador = typeof opcoes[0];
+
+interface Props {
+  ordenador: string,
+  setOrdenador: React.Dispatch<React.SetStateAction<string>>
+}
+
+
+export default function Ordernador({ordenador, setOrdenador}: Props) {
+    const [aberto, setAberto] = useState(false)
+    const nomeOrdenador = ordenador && opcoes.find((opcao) => opcao.value === ordenador)?.nome;
     return (
-        <button className={styles.ordenador}>
-            <span>Ordenar Por</span>
-            <div className={styles.ordenador__options}>
-                {opcoes.map((opcao) => (
-                    <div className={styles.ordenador__option} key={opcao.value}>
-                        {opcao.nome}
-                    </div>
-                ))}
-            </div>
+        <button 
+            className={classNames({
+                [styles.ordenador]: true,
+                [styles['ordenador--ativo']]: ordenador !== "",
+            })} 
+            onClick={() => setAberto(!aberto)}
+            onBlur={() => setAberto(false)} 
+        >
+        <span>{nomeOrdenador || "Ordenar Por"}</span>
+        {aberto? < MdKeyboardArrowUp size={20}/> : < MdKeyboardArrowDown size={20} />}
+        <div className={classNames({
+            [styles.ordenador__options]: true,
+            [styles['ordenador__options--ativo']]: aberto
+        })}>
+            {opcoes.map((opcao) => (
+                <div 
+                className={styles.ordenador__option} 
+                key={opcao.value}
+                onClick={() => setOrdenador(opcao.value)}>
+                    {opcao.nome}
+                </div>
+            ))}
+        </div>
         </button>
     )
 }
