c8a6e3c24ac1519b6413dd52de8f5d95100ba8d4
Filipe <eduardojs999@gmail.com>
2022-11-08 12:02:59 -0300

    adding recommendations menu

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'normalize.css';
 import Cardapio from './pages/Cardapio';
+import Inicio from './pages/Inicio';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Cardapio />
+		<Inicio/>
+
 	</React.StrictMode>,
 	document.getElementById('root')
 );
