2ca67d85b55f0f249dd0ac38462a7907add7a794
Filipe <eduardojs999@gmail.com>
2022-11-13 10:54:44 -0300

    footer criado

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import Footer from 'components/Footer';
 import Header from 'components/Header';
 import Menu from 'components/Menu';
 import Cardapio from 'pages/Cardapio';
@@ -18,6 +19,7 @@ export default function AppRouter() {
                         <Route path='sobre' element={<Sobre />}></Route>
                     </Route>
                 </Routes>
+                <Footer/>
             </Router>
         </main>
 
