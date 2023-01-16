35b362ae2f59a177ff3ad833732bb115523f1dc3
Filipe Sales <eduardojs999@gmail.com>
2023-01-16 13:16:21 -0500

    finalizando projeto

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,39 +1,34 @@
-import styles from './Prato.module.scss';
-import { useNavigate, useParams } from 'react-router-dom';
-import cardapio from 'data/cardapio.json';
-import TagsPrato from 'components/TagsPrato';
-import NotFound from 'pages/NotFound';
-import Header from 'components/Header';
+import styles from "./Prato.module.scss";
+import { useNavigate, useParams } from "react-router-dom";
+import cardapio from "data/cardapio.json";
+import Header from "components/Header";
+import { lazy } from "react";
+
+const TagsPrato = lazy(() => import("components/TagsPrato"));
+const NotFound = lazy(() => import("pages/NotFound"));
 
 export default function Prato() {
-    const { id } = useParams();
-    const navigate = useNavigate();
-    const prato = cardapio.find(item => item.id === Number(id)); 
-    if (!prato) {
-        return <NotFound/>;
-    }
-    return (
-        <Header>
-            <button
-                className={styles.voltar}
-                onClick={() => (navigate(-1))}
-            >
-                {'< Voltar'}
-            </button>
-            <section className={styles.container}>
-                <h1 className={styles.titulo}>
-                    {prato.title}
-                </h1>
-                <div className={styles.imagem}>
-                    <img src={prato.photo} alt={prato.title} />
-                </div>
-                <div className={styles.conteudo}>
-                    <p className={styles.conteudo__descricao}>
-                        {prato.description}
-                    </p>
-                    <TagsPrato {...prato}/>
-                </div>
-            </section>
-        </Header>
-    );
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const prato = cardapio.find((item) => item.id === Number(id));
+  if (!prato) {
+    return <NotFound />;
+  }
+  return (
+    <Header>
+      <button className={styles.voltar} onClick={() => navigate(-1)}>
+        {"< Voltar"}
+      </button>
+      <section className={styles.container}>
+        <h1 className={styles.titulo}>{prato.title}</h1>
+        <div className={styles.imagem}>
+          <img src={prato.photo} alt={prato.title} />
+        </div>
+        <div className={styles.conteudo}>
+          <p className={styles.conteudo__descricao}>{prato.description}</p>
+          <TagsPrato {...prato} />
+        </div>
+      </section>
+    </Header>
+  );
 }
