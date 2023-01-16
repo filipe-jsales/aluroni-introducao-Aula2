9716633a7eff1357fde47c8c995642b6a2c9462f
Filipe Sales <eduardojs999@gmail.com>
2023-01-16 07:28:04 -0500

    feat: using memo on Buscador

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import styles from './Buscador.module.scss';
-import { CgSearch } from 'react-icons/cg';
+import React, { memo } from "react";
+import styles from "./Buscador.module.scss";
+import { CgSearch } from "react-icons/cg";
 
-interface Props{
-    busca: string,
-    setBusca: React.Dispatch<React.SetStateAction<string>>
+interface Props {
+  busca: string;
+  setBusca: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default function Buscador({busca, setBusca}: Props) {
-	return <div className={styles.buscador}>
-		<input
-			value={busca}
-			onChange={evento => setBusca(evento.target.value)}
-			placeholder={'Buscar'}
-		/>
-		<CgSearch size={20} color="#4C4D5E"/>
-	</div>;
-} 
+function Buscador({ busca, setBusca }: Props) {
+  return (
+    <div className={styles.buscador}>
+      <input
+        value={busca}
+        onChange={(evento) => setBusca(evento.target.value)}
+        placeholder={"Buscar"}
+      />
+      <CgSearch size={20} color="#4C4D5E" />
+    </div>
+  );
+}
+export default memo(Buscador);
